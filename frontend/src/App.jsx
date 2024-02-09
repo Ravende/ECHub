c787419a7bedebd3b8626079a507a267c9ea4038
M.Y.KIM <ravenade88@gmail.com>
2024-02-09 19:10:03 +0900

    docs: Update .eslintrc.json

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import cafeBox from './cafeBox';
+
+function App() {
+  return (
+    <>
+      <header>
+        <div id="logo">
+          <h2>1 project</h2>
+        </div>
+        <div id="siteName">
+          <h1>Ewha w. univ cafe Map</h1>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default App;
