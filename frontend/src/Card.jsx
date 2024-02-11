7b417cd6a6c924170fb5389468cdd1f532dcd086
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-02-12 00:58:14 +0900

    [FE] feat: aside 수정

---
+++ frontend/src/Card.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+export default function Card(props) {
+  return (
+    <div className="cafe-box">
+      
+      <div className="cafe-name">카페명</div>
+      <div className="operating-hours">영업시간 : 09:00-20:00</div>
+      <div className="operating-state">영업상태 : 영업중</div>
+      <div className="menu">대표메뉴 : 생과일주스 </div>
+      
+      <div className="cafe-image"  >
+        <img className="cafeimg" alt="cafeimg1" src ="cafeimage.jpg"  />
+      </div>
+      
+      <div className="hash">
+        #포토존</div>
+      </div>
+      
+  );
+}
