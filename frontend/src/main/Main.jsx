ba85bb13bae036b6a6917aa9adc035d621551459
hanseul-lee <hanseul.dev@gmail.com>
2024-02-12 17:09:55 +0900

    feat: 지도 API 연동을 위한 설치 및 리팩토링

--- frontend/src/main/Main.jsx
+++ frontend/src/main/Main.jsx
@@ -1,11 +1,13 @@
 import { Header } from '../header';
 import { SideBar } from '../sideBar';
+import { KakaoMap } from '../map';
 
 export function Main() {
   return (
     <>
       <Header />
       <SideBar />
+      <KakaoMap />
     </>
   );
 }
