5b2ea0495745471e26cf5984c9866728925e6ade
M.Y.KIM <ravenade88@gmail.com>
2024-02-18 04:01:39 +0900

    [FE] feat: 리액트로 map 마커 생성, 인포윈도우 생성, 클릭 이벤트 추가, 마우스오버 이벤트 추가

--- frontend/src/map/KakaoMap.jsx
+++ frontend/src/map/KakaoMap.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Map, MapInfoWindow, MapMarker } from 'react-kakao-maps-sdk';
-import { useRef } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { useRef, useState } from 'react';
 import useKakaoLoader from './useKakaoLoader.jsx';
 
 export default function KakaoMap() {
@@ -8,6 +8,8 @@ export default function KakaoMap() {
 
   // eslint-disable-next-line
   const mapRef = useRef(null); /* prettier-ignore */
+
+  const [isOpen, setIsOpen] = useState(false);
 
   /* 지도 확대, 축소 버튼 */
   const zoomIn = () => {
@@ -22,56 +24,58 @@ export default function KakaoMap() {
   };
 
   return (
-    <div className={'section'}>
-      <div className={'map_wrap'}>
-        {/* 지도 생성 */}
-        <Map // 지도를 표시할 Container
-          id="map"
-          center={{
+    <div className={'map_wrap'}>
+      {/* 지도 생성 */}
+      <Map // 지도를 표시할 Container
+        id="map"
+        center={{
+          lat: 37.559716,
+          lng: 126.945468,
+        }}
+        style={{
+          width: '100%',
+          height: '100vh',
+          position: 'relative',
+          overflow: 'hidden',
+        }}
+        level={3}
+        ref={mapRef}>
+        {/* 지도 확대, 축소 컨트롤 div */}
+        <div className="custom_zoomcontrol radius_border">
+          <span onClick={zoomIn}>
+            <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png" alt="확대" />
+          </span>
+          <span onClick={zoomOut}>
+            <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png" alt="축소" />
+          </span>
+        </div>
+
+        {/* 마커 생성 */}
+        <MapMarker
+          position={{
+            // 마커 위치
             lat: 37.559716,
             lng: 126.945468,
           }}
-          style={{
-            width: '100%',
-            height: '600px',
-            position: 'relative',
-            overflow: 'hidden,',
-          }}
-          level={3}
-          ref={mapRef}>
-          {/* 지도 확대, 축소 컨트롤 div */}
-          <div className="custom_zoomcontrol radius_border">
-            <span onClick={zoomIn}>
-              <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png" alt="확대" />
-            </span>
-            <span onClick={zoomOut}>
-              <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png" alt="축소" />
-            </span>
-          </div>
-
+          removable={true}
+          clickable={true}
+          onClick={() => setIsOpen(true)} // 클릭 이벤트
+          onMouseOver={() => setIsOpen(true)} // 마우스오버 이벤트
+          onMouseOut={() => setIsOpen(false)}>
           {/* 인포윈도우 생성 */}
-          <MapInfoWindow
-            position={{
-              // 인포윈도우 위치
-              zIndex: 1,
-            }}
-            removable={true} // 인포윈도우 x버튼 표시
-          >
-            {/* 인포윈도우에 표출될 내용으로 HTML 문자열이나 React Component가 가능합니다 */}
-            <div style={{ padding: '5px', color: '#000' }}>plcae.place_name</div>
-          </MapInfoWindow>
-
-          {/* 마커 생성 */}
-          <MapMarker
-            position={{
-              // 마커 위치
-              lat: 37.559716,
-              lng: 126.945468,
-            }}>
-            {/* MapMarker의 자식을 넣어줌으로 해당 자식이 InfoWindow로 만들어지게 합니다 */}
-          </MapMarker>
-        </Map>
-      </div>
+          {isOpen && (
+            <div className="InfoWindow">
+              <h2 style={{ lineHeight: '2.0' }}>카페명</h2>
+              <p style={{ lineHeight: '1.0' }}>
+                월~금: 12:00~22:00
+                <br />
+                토,일: 11:00~22:00
+              </p>
+              <p style={{ lineHeight: '1.8', color: 'blue' }}>영업중</p>
+            </div>
+          )}
+        </MapMarker>
+      </Map>
     </div>
   );
 }
