bf1bb52d0e3dbc9a6d8245c5c656756b29312151
M.Y.KIM <ravenade88@gmail.com>
2024-02-17 22:05:51 +0900

    [FE] fix: jsx 파일 useRef 버그 수정하고 구현

--- frontend/src/map/KakaoMap.jsx
+++ frontend/src/map/KakaoMap.jsx
@@ -1,15 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapInfoWindow, MapMarker } from 'react-kakao-maps-sdk';
 import { useRef } from 'react';
 import useKakaoLoader from './useKakaoLoader.jsx';
-
-const { kakao } = window;
 
 export default function KakaoMap() {
   useKakaoLoader();
 
   // eslint-disable-next-line
-  const mapRef = useRef < kakao.maps.Map > (null); /* prettier-ignore */
+  const mapRef = useRef(null); /* prettier-ignore */
 
   /* 지도 확대, 축소 버튼 */
   const zoomIn = () => {
@@ -26,32 +24,53 @@ export default function KakaoMap() {
   return (
     <div className={'section'}>
       <div className={'map_wrap'}>
+        {/* 지도 생성 */}
         <Map // 지도를 표시할 Container
           id="map"
           center={{
-            // 지도의 중심좌표
             lat: 37.559716,
             lng: 126.945468,
           }}
           style={{
-            // 지도의 크기
             width: '100%',
             height: '600px',
             position: 'relative',
             overflow: 'hidden,',
           }}
-          level={3} // 지도의 확대 레벨
-          ref={mapRef}></Map>
-      </div>
+          level={3}
+          ref={mapRef}>
+          {/* 지도 확대, 축소 컨트롤 div */}
+          <div className="custom_zoomcontrol radius_border">
+            <span onClick={zoomIn}>
+              <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png" alt="확대" />
+            </span>
+            <span onClick={zoomOut}>
+              <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png" alt="축소" />
+            </span>
+          </div>
 
-      {/* 지도 확대, 축소 컨트롤 div */}
-      <div className="custom_zoomcontrol radius_border">
-        <span onClick={zoomIn}>
-          <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_plus.png" alt="확대" />
-        </span>
-        <span onClick={zoomOut}>
-          <img src="https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/ico_minus.png" alt="축소" />
-        </span>
+          {/* 인포윈도우 생성 */}
+          <MapInfoWindow
+            position={{
+              // 인포윈도우 위치
+              zIndex: 1,
+            }}
+            removable={true} // 인포윈도우 x버튼 표시
+          >
+            {/* 인포윈도우에 표출될 내용으로 HTML 문자열이나 React Component가 가능합니다 */}
+            <div style={{ padding: '5px', color: '#000' }}>plcae.place_name</div>
+          </MapInfoWindow>
+
+          {/* 마커 생성 */}
+          <MapMarker
+            position={{
+              // 마커 위치
+              lat: 37.559716,
+              lng: 126.945468,
+            }}>
+            {/* MapMarker의 자식을 넣어줌으로 해당 자식이 InfoWindow로 만들어지게 합니다 */}
+          </MapMarker>
+        </Map>
       </div>
     </div>
   );
