df15b8238be03d75a94e3846403813fc56a13b7b
M.Y.KIM <ravenade88@gmail.com>
2024-02-18 04:00:48 +0900

    [FE] fix: useKakaoLoader.jsx 라이브러리 수정

--- frontend/src/map/useKakaoLoader.jsx
+++ frontend/src/map/useKakaoLoader.jsx
@@ -4,6 +4,6 @@ import { useKakaoLoader as useKakaoLoaderOrigin } from 'react-kakao-maps-sdk';
 export default function useKakaoLoader() {
   useKakaoLoaderOrigin({
     appkey: '843de9881c04c74982ab3924b1e63164',
-    libraries: ['services'],
+    libraries: ['clusterer', 'drawing', 'services'],
   });
 }
