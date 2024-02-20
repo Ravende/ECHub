b3b2c5ae5ba8494293d81a785e424ec575bed59f
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-02-20 15:56:54 +0900

    [fe] feat: 수정

--- frontend/src/cardInfo/CardInfo.jsx
+++ frontend/src/cardInfo/CardInfo.jsx
@@ -17,29 +17,22 @@ export function CardInfo() {
       </div>
       <div id="operating-hours">평일 12:0~22:00 / 주말 11:00~22:00</div>
       
-      {/* 리뷰 확인하는 box
-       <div className="review-box">
-       
-       <div id = "review-name" >평점</div> 
-       
-       <div className="review-wrap">
-        <div className="review">
-          <div id = "review-text">리뷰</div>
-          <div id = "text">멀티탭이 많아요</div>
+      <div className="review-box">
+        {/* <img id= "cof_icon" alt = "cof" src = "./assets/cofimg.png" />
+         */}
+        <div id="review-name">평점</div>
+        <div className="review-wrap">
+          <div className="review">
+            <h3>리뷰</h3>
+            <h4>멀티탭이 많아요</h4>
+          </div>
+          {/* <div className="review">
+          <h3>리뷰</h3>
+          <h4>멀티탭이 많아요</h4>
+        </div> */}
         </div>
       </div>
-     </div> */}
-     { /* 리뷰 쓰는 칸 */}
-      <div className= "write-box">
-        <div id = "review-name">평점</div>
-        <div className = "write">
-
-          <div id = "write-text">자리가 편해서 오래 공부하기 좋아요.</div>
-        </div>
-
-      </div>
-      <div id="menu">대표메뉴 </div>
-
+      <div id="menu">대표메뉴</div>
       <div className="menulist-wrap">
         <img id="menulist" alt="menu" src="./assets/americano.png" />
         <img id="menulist" alt="menu" src="./assets/americano.png" />
