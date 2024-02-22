c1424ecafbe40101f62d3d1087eebf9a7e0600aa
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-02-22 22:43:54 +0900

    [FE] feat: review->write 컴포넌트 전환 추가

--- frontend/src/header/Header.jsx
+++ frontend/src/header/Header.jsx
@@ -3,16 +3,6 @@ import coffeeImage from './coffee.png';
 import logo from './LogoWhite.png';
 
 export function Header() {
-  // const [isPopupOpen, setPopupOpen] = useState(false);
-
-  // const openPopup = () => {
-  //   setPopupOpen(true);
-  // };
-
-  // const closePopup = () => {
-  //   setPopupOpen(false);
-  // };
-
   return (
     <header>
       <div id="logo">
@@ -29,33 +19,10 @@ export function Header() {
       </div>
       <div id="container">
         <button className="loginBtn">로그인</button>
+        
         <button className="joinBtn">회원가입</button>
       </div>
 
-      {/* {isPopupOpen && (
-        <div className="loginPopup">
-          <div className="modal-content">
-            <span className="close" onClick={closePopup}>
-              &times;
-            </span>
-            <h2>로그인</h2>
-            <div id="loginForm">
-              <form>
-                <ul>
-                  <li>
-                    <label for="name">이 름</label>
-                    <input type="text" id="name" />
-                  </li>
-                  <li>
-                    <label for="phone">비밀번호</label>
-                    <input type="password" id="phone" />
-                  </li>
-                </ul>
-              </form>
-            </div>
-          </div>
-        </div>
-      )} */}
-    </header>
+     </header>
   );
 }
